54d99066c03d6df0e4b68ca30505e3fb82b11270
Akhele <Moneimb8@gmail.com>
2021-05-23 18:38:37 +0100

    Activate the Hot Reload function

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
+  runApp(
+    contact()
+  );
+}
+
+// ignore: camel_case_types
+class contact extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
         appBar: AppBar(
           title: Text("Contact Us"),
           backgroundColor: Colors.blueAccent,
 
         ),
-    ),
-  ));
+      ),
+    );
+  }
 }
-
